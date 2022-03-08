a86865da6a898ce8ea235aa92864676dc5267e1f
DESKTOP\nikigmi <nikigmy@gmail.com>
2022-03-08 22:12:10 +0200

    test 24

--- BuildScripts/GatherDependencies.sh
+++ BuildScripts/GatherDependencies.sh
@@ -6,4 +6,4 @@ rm -rfv *.dll
 curl -sJLH 'Authorization:token '"$GIHUB_TOKEN"'' "https://api.github.com/repos/nikigmy/LibratyTest/releases/latest" \
 | grep 'releases/assets' \
 | cut -d '"' -f 4 \
-| xargs -n 1 curl -JL -O -H 'Authorization:token '"$GIHUB_TOKEN"'' -H "Accept:application/octet-stream" -C - \
+| xargs -n 1 curl -JL -O -H 'Authorization:token '"$GIHUB_TOKEN"'' -H "Accept:application/octet-stream" 
